35e9258cee9cdf204bef84773b4f59e9b018c4aa
gluehloch <gluehloch@googlemail.com>
2023-09-09 15:41:44 +0200

    ...

--- src/test/java/de/awtools/keystore/CryptoniumTest.java
+++ src/test/java/de/awtools/keystore/CryptoniumTest.java
@@ -14,16 +14,18 @@ class CryptoniumTest {
         KeyStoreService keyStoreService = new KeyStoreService(ks, "awtest", "awtest");
         var c = new Cryptonium(keyStoreService);
         
-        byte[] encrypt = c.encrypt("Das ist ein Test".getBytes("UTF-8"));
+        byte[] decrypt = c.decrypt("Das ist ein Test".getBytes("UTF-8"));
+        System.out.println(decrypt);
+        
+        byte[] encrypt = c.decrypt(decrypt);
         System.out.println(encrypt);
         
-        c.decrypt(encrypt);
-        System.out.println(encrypt);
-        
+        /*
         byte[] encrypt2 = c.encrypt("Das ist ein Test".getBytes("UTF-8"));
         System.out.println(encrypt2);
         
         byte[] decrypt2 = c.decrypt(encrypt2);
         System.out.println(decrypt2);
+        */
     }
 }
